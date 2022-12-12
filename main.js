417f11be7076a68b17866e996d09fe405791bd6a
Your Name <you@example.com>
2022-12-11 23:02:58 -0300

    change

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-function Information() {
-    var name = info.name.value;
-    var mail = info.E-MAIL.value;
-    var ASSUNTO = info.ASSUNTO.value;
-    var MESSAGE= info.MESSAGE.value;
-
-document.writeln("<table>");
-document.writeln("<caption> <h3>information in inter <h3></caption>"); 
-document.writeln("<tr><td> <b>name: </b></td> <td>" + name + " </td> </tr>");
-document.writeln("<tr><td> <b>E-MAIL: </b></td> <td>" + mail + " </td> </tr>");
-document.writeln("<tr><td> <b>ASSUNTO: </b></td> <td>" + ASSUNTO + " </td> </tr>");
-document.writeln("<tr><td> <b>MESSAGE: </b></td> <td>" + MESSAGE + " </td> </tr>");
-document.writeln("<table>");
-
-
-
+function valida( ) {
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+    if (usuario == ""){
+        document.getElementById("eusuario").innerHTML = "Vazio.";
+        document.getElementById("esenha").innerHTML = "";
+    }
+    else if (senha == "") {
+        document.getElementById("eusuario").innerHTML = "";
+        document.getElementById("esenha").innerHTML = "Vazio.";
+    }
+    else if (senha.length < 6) {
+        document.getElementById("eusuario").innerHTML = "";
+        document.getElementById("esenha").innerHTML = "Senha muito curta!";
+    }
+    else {
+        document.getElementById("eusuario").innerHTML = "";
+        document.getElementById("esenha").innerHTML = "";
+        document.getElementById("msg").innerHTML = "Formulário validado!";            
+    }		
 }
